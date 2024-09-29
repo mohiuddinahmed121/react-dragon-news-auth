f271d3015fb37a79ed0b164cb4e822053cefdf4b
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2024-09-29 09:16:11 +0600

    first commit

---
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="space-y-6">
+      <h2 className="text-2xl">All categories</h2>
+      {categories.map((category) => (
+        <Link
+          className="block ml-4 text-xl font-semibold"
+          key={category.id}
+          to={`/category.id`}
+        >
+          {category.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default LeftSideNav;
